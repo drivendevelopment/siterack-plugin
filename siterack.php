c46575f600c1c5f60aad764c00ff510cb5bb1ce2
Vinny McKee <vinny@drivendevelopment.com>
2023-08-26 10:10:41 -0700

    Attempt to fix deploy workflow

--- siterack.php
+++ siterack.php
@@ -4,7 +4,7 @@
 Plugin Name: SiteRack
 Plugin URI: https://siterack.app
 Description: Manage all your WordPress sites from a single dashboard with SiteRack.
-Version: 0.0.7
+Version: 0.0.8
 Author: Site Rack
 Author URI: https://siterack.app
 Text Domain: siterack
