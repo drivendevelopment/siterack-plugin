a864279ae7f8047171abc6639a36ba59d3e1603a
Vinny McKee <vinny@drivendevelopment.com>
2023-09-07 13:05:57 -0700

    Fix indentation and add comments

--- classes/rest-api/v1/class-rest-api.php
+++ classes/rest-api/v1/class-rest-api.php
@@ -23,8 +23,7 @@ class REST_API extends WP_REST_Controller {
     private $token_error;
 
     public function __construct() {
-        add_filter( 'determine_current_user', 	array( $this, 'determine_current_user' ) );
-        //add_filter( 'rest_pre_dispatch', 		array( $this, 'rest_pre_dispatch' ) );
+        add_filter( 'determine_current_user', array( $this, 'determine_current_user' ) );
 
         $this->register_routes();
     }
@@ -32,12 +31,12 @@ class REST_API extends WP_REST_Controller {
     public function register_routes() {
         register_rest_route( $this->base, '/login-url', array(
             array(
-                'methods'             => WP_REST_Server::CREATABLE,
-                'callback'            => array( $this, 'login_url' ),
-                'permission_callback' => array( $this, 'login_url_permission_callback' ),
+                'methods'               => WP_REST_Server::CREATABLE,
+                'callback'              => array( $this, 'login_url' ),
+                'permission_callback'   => array( $this, 'login_url_permission_callback' ),
                 'args' => array(
                     'user_id' => array(
-                        'default' 	=> '',
+                        'required' => true,
                     )
                 ),
             ),
@@ -45,24 +44,24 @@ class REST_API extends WP_REST_Controller {
 
         register_rest_route( $this->base, '/users', array(
             array(
-                'methods'             => WP_REST_Server::READABLE,
-                'callback'            => array( $this, 'users' ),
-                'permission_callback' => array( $this, 'has_valid_token' ),
+                'methods'               => WP_REST_Server::READABLE,
+                'callback'              => array( $this, 'users' ),
+                'permission_callback'   => array( $this, 'has_valid_token' ),
             ),
         ) );	
         
         register_rest_route( $this->base, '/users/token', array(
             array(
-                'methods'             => WP_REST_Server::CREATABLE,
-                'callback'            => array( $this, 'user_token' ),
-                'permission_callback' => '__return_true',
+                'methods'               => WP_REST_Server::CREATABLE,
+                'callback'              => array( $this, 'user_token' ),
+                'permission_callback'   => '__return_true',
                 'args' => array(
                     'username' => array(
-                        'required' 			=> true,
+                        'required'          => true,
                         'sanitize_callback' => 'sanitize_text_field',
                     ),
                     'password' => array(
-                        'required' 			=> true,
+                        'required'          => true,
                         'sanitize_callback' => 'sanitize_text_field',
                     ),					
                 ),				
@@ -120,53 +119,70 @@ class REST_API extends WP_REST_Controller {
         return $request;
     }
 
+    /**
+     * Ensure that the user ID that a login URL is being requested for matches
+     * the user ID in the authenticaiton token.
+     */
     public function login_url_permission_callback( WP_REST_Request $request ) {
         $user_id = $request->get_param( 'user_id' );
 
         return get_current_user_id() == $user_id;
     }
 
+    /**
+     * Checks that an authentication token is present and valid.
+     */
     public function has_valid_token( WP_REST_Request $request ) {
         $json_web_token = new JSON_Web_Token();
-        $token 			= $json_web_token->get_token_from_header();
-        $token 			= $json_web_token->validate( $token );
+        $token          = $json_web_token->get_token_from_header();
+        $token          = $json_web_token->validate( $token );
 
         return ! is_wp_error( $token );
     }
 
+    /**
+     * Returns a URL that can be used to log the user into the site.
+     */
     public function login_url( WP_REST_Request $request ) {
-        $user_id 	= $request->get_param( 'user_id' );
-        $token 		= bin2hex( random_bytes( 32 ) );
+        $user_id    = $request->get_param( 'user_id' );
+        $token      = bin2hex( random_bytes( 32 ) );
 
         update_user_meta( $user_id, 'siterack_login_token', $token );
         update_user_meta( $user_id, 'siterack_login_token_expiration', strtotime( '+1 minute' ) );
         
         $url = add_query_arg( array(
-            'action' 	=> 'siterack_login',
-            'token' 	=> $token,
+            'action'    => 'siterack_login',
+            'token'     => $token,
         ), get_site_url() );
 
         return $url;
     }
 
+    /**
+     * Returns the site's users.
+     */
     public function users() {
         $users = get_users( array(
             'fields' => array( 'ID', 'user_login', 'user_email', 'display_name' ),
         ) );
 
         foreach ( $users as &$user ) {
-            $user->avatar_url 	= get_avatar_url( $user->ID );
-            $user->first_name 	= get_user_meta( $user->ID, 'first_name', true );
-            $user->last_name 	= get_user_meta( $user->ID, 'last_name', true );
+            $user->avatar_url   = get_avatar_url( $user->ID );
+            $user->first_name   = get_user_meta( $user->ID, 'first_name', true );
+            $user->last_name    = get_user_meta( $user->ID, 'last_name', true );
         }
 
         return $users;
     }
 
+    /**
+     * Checks the user's credentials and, if valid, returns a JSON web token that
+     * can be used to authenticate future requests as the user.
+     */
     public function user_token( WP_REST_Request $request ) {
-        $username 	= $request->get_param( 'username' );
-        $password 	= $request->get_param( 'password' );
-        $user 		= wp_authenticate( $username, $password );
+        $username   = $request->get_param( 'username' );
+        $password   = $request->get_param( 'password' );
+        $user       = wp_authenticate( $username, $password );
 
         if ( is_wp_error( $user ) ) {
             return new WP_Error(
@@ -176,7 +192,7 @@ class REST_API extends WP_REST_Controller {
             );
         } else {
             $json_web_token = new JSON_Web_Token();
-            $token 			= $json_web_token->generate( $user->ID );
+            $token          = $json_web_token->generate( $user->ID );
 
             return array(
                 'token' => $token,
